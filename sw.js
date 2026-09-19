fce2bb95f17b935eb4c8635c922579ad5390f495
bfbcsongs <paulm4035@gmail.com>
2026-09-19 20:02:18 +0800

    Add service worker for caching assets
    Implement service worker for caching and offline support

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'miki-calc-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html'
+];
+
+// Install the service worker and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch cached files when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
